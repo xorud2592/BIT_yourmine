869d81d5539268e6cdb734c389ea3adf714985ba
Kminseung <soccer6504@gmail.com>
2021-06-17 10:54:00 +0900

    Implement Search Function

---
+++ src/main/java/com/example/springboot/web/IndexController.java
@@ -0,0 +1,36 @@
+package com.example.springboot.web;
+
+import com.example.springboot.domain.posts.Posts;
+import com.example.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+    // Posts Searching
+    @GetMapping("/posts/search")
+    public String search(String keyword, Model model) {
+        List<Posts> searchList = postsService.search(keyword);
+        model.addAttribute("searchList", searchList);
+        return "posts-search";
+    }
+}
